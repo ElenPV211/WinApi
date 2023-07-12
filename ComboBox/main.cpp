a3f0477ba60bfb5b865b6597654f4d1f4bedd57b
user <user@DESKTOP-LI69LEU>
2023-07-12 21:35:32 +0300

    ComboBox

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -1,6 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
+#include"resource.h"
+
+CONST CHAR* values[] = { "This","is","my","First","Combo","Box"};
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	MessageBox(NULL, "ComboBox check", "Info", MB_OK | MB_ICONINFORMATION);
+	//MessageBox(NULL, "ComboBox check", "Info", MB_OK | MB_ICONINFORMATION);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+
+		for (int i = 0; i < sizeof(values) / sizeof(values[0]);i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)values[i]);
+		}
+	}
+		break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);//getcursrl-определяем номер 
+			//выбранного элемента выпадающего списка
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+			CHAR sz_message[SIZE] = {}; 
+			sprintf(sz_message, "Вы выбрали элемент №%i, со значением \"%s\"", i, sz_buffer);
+			MessageBox(hwnd, sz_message, "info", MB_OK | MB_ICONINFORMATION);
+		}
+			break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+	}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
 }
--- WinApi/main.cpp
+++ WinApi/main.cpp
@@ -1,12 +1,14 @@
 ﻿#include<Windows.h>
 #include"resource.h"
 
+CONST CHAR g_sz_LOGIN_INVITE[] = "Введите имя пользователя";
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	//MessageBox(NULL, "Привет мир! Я окно сообщения:-) ", "Info", MB_YESNOCANCEL | MB_ICONINFORMATION);
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
-	
+
 	return 0;
 }
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -17,12 +19,32 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		HWND hLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SendMessage(hLogin, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITE);
 	}	break;
 	case WM_COMMAND:   //обрабатываем команды нажатия на кнопки, наведение мыши...
 		switch (LOWORD(wParam))
 		{
+		case IDC_EDIT_LOGIN:  // ID окна к которому обращаемся
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (HIWORD(wParam) == EN_SETFOCUS)
+			{
+				if (strcmp(sz_buffer, g_sz_LOGIN_INVITE) == 0)
+					SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+			}
+			if (HIWORD(wParam) == EN_KILLFOCUS)
+			{
+				if (strlen(sz_buffer) == 0)
+					SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITE);
+			}
+		}
+		break;
 		case IDC_BUTTON_COPY:
-		{ 
+		{
 			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);//получение хендлера окна
 			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
 			const int SIZE = 256;
@@ -30,16 +52,16 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-			SendMessage(GetDlgItem(hwnd,IDC_STATIC_LOGIN), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(GetDlgItem(hwnd, IDC_STATIC_LOGIN), WM_SETTEXT, 0, (LPARAM)sz_buffer);
 
 		}
-			break;
-		case IDOK: MessageBox(hwnd, "была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION);
 		break;
+		case IDOK: MessageBox(hwnd, "была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION);
+			break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
 		break;
-	case WM_CLOSE: EndDialog(hwnd,0);
+	case WM_CLOSE: EndDialog(hwnd, 0);
 	}
 	return FALSE;
 }
