90837813033982d24b378cc77d681d3138e79733
user <user@DESKTOP-LI69LEU>
2023-07-17 19:19:58 +0300

    ListBox

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -1,9 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
 #include"resource.h"
 
-CONST CHAR* values[] = { "This","is","my","second","Combo","Box" };
+CONST CHAR* values[] = { "This","is","my","first","List","Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR IpCmdLine, INT nCmdShow)
 {
@@ -18,23 +21,79 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
-		break;
-		HWND hSTR = GetDlgItem(hwnd, IDC_LIST_STR);
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
 		for (int i = 0; i < sizeof(values) / sizeof(values[0]); i++)
 		{
-			SendMessage(hSTR, WM_GETTEXT, 0, (LPARAM)values[i]);
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)values[i]);
 		}
 	}
+	break;
 	case WM_COMMAND:
 	{
-	//case IDOK: 
-	// 
-		//break;
-	case IDCANCEL: EndDialog(hwnd, 0); 
-	}
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_ADD:
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAdd, 0);
+			break;
+		case IDC_BUTTON_DELETE:
+		{
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+
+			SendMessage(hList, LB_DELETESTRING, SendMessage(hList, LB_GETCURSEL, 0, 0), 0);
+		}
 		break;
+		case IDOK:
+		{
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			int i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			CHAR sz_message[SIZE] = {};
+			sprintf(sz_message, "Вы выбрали элемент №%i, со значением \"%s\"", i, sz_buffer);
+			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+		}
+		break;
+		case IDCANCEL: EndDialog(hwnd, 0);
+		}
+		break;
+	}
+	break;
 	case WM_CLOSE: EndDialog(hwnd, 0);
 
+	}
+	return FALSE;
+}
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT_ADD), WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			HWND hParent = GetParent(hwnd); //получаем родительское окно
+			HWND hList = GetDlgItem(hParent, IDC_LIST1);
+			
+			if (SendMessage(hList, LB_FINDSTRING, 0, (LPARAM)sz_buffer) == LB_ERR)
+				//LB_FINDSTRING -Находит первую строку в списке, начинающуюся с указанной строки.
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			else
+				MessageBox(hwnd, "Такая запись уже существует", "Info", MB_OK | MB_ICONINFORMATION);
+		}
+		//break;
+		case IDCANCEL:EndDialog(hwnd, 0);
+		}
+	}
+	break;
+	case WM_CLOSE:  EndDialog(hwnd, 0);
 	}
 	return FALSE;
 }
---
+++ MainWindo/main.cpp
@@ -0,0 +1,95 @@
+#include<Windows.h>
+
+CONST CHAR g_sz_MY_WINDOW_CLASS[] = "MyFirstWindow";
+
+INT CALLBACK WndProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam);//процедура окна
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR IpCmdLine, INT nCmdShow)
+{
+	//1.Регистрация класса окна
+	 WNDCLASSEX wc; //объявляем класс структуры WNDCLASSEX с именем wc
+	 //зануляем экземпляр класса окна wc функцией
+	 ZeroMemory(&wc, sizeof(wc));
+
+     //инициализируем экземпляр класса
+	 //поля связанные с размерами
+	 wc.cbSize = sizeof(wc); //размер класса в Байтах cb-Count Bytes
+	 wc.cbClsExtra = 0; //Class ExtraBytes дополнительные байты класса
+	 wc.cbWndExtra = 0; //WindowExtraBytes дополнительные байты окна
+	 wc.style = 0;
+
+	 //внешний вид окна, иконки и курсоры
+	 wc.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	 wc.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+	 wc.hCursor = LoadCursor(hInstance, IDC_ARROW);
+	 wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+	 wc.hInstance = hInstance;
+	 wc.lpfnWndProc = (WNDPROC)WndProc; //процедура окна
+	 wc.lpszMenuName = NULL;
+	 wc.lpszClassName = g_sz_MY_WINDOW_CLASS;//строка без которой выходит ошибка класс не зарегестрирован
+	 //lpsz(long pointer saiz zero) - указатель на строку
+
+
+	 //функция для регистрации структуры RegisterClassEx. проверка зарегестрирован ли класс
+	 if (!RegisterClassEx(&wc))
+	 {
+		 MessageBox(NULL, "Класс не зарегестрирован", "Error", MB_OK | MB_ICONERROR);
+		 return 0;
+	 }
+	//2 Создание окна
+	 //вызываем функцию CreateWindowEx
+	 HWND hwnd = CreateWindowEx
+	 (
+		 0,
+		 g_sz_MY_WINDOW_CLASS,//имя класса окна 
+		 g_sz_MY_WINDOW_CLASS,//заголовок окна
+		 WS_OVERLAPPEDWINDOW,//за стиль окна отвечает класс окна и для каждого класса есть свой набор стилей
+		//вы создаёте окно или кнопку или дрек даун, стиль окна определяется его классом
+//Стиль окна для главного окна программы всегда будет WS_OVERLAPPEDWINDOW
+		//WS_OVERLAPPEDWINDOW - это окно верхнего уровня (TOP_LEVEL_WINDOW), которое включает в себя дочерние окна (CHILDWINDOW)
+	
+		 CW_USEDEFAULT,CW_USEDEFAULT, //начальная позиция окна (х,у положение на экране)
+		 CW_USEDEFAULT,CW_USEDEFAULT, //размер окна (ширина, высота)
+		 NULL,//Родительское окно, Parametr Window
+		 NULL,//для главного окна это меню, для дочернего окна это ID его ресурса
+		 hInstance,
+		 NULL		
+	 );
+	 if (hwnd == 0)
+	 {
+		 MessageBox(NULL, "окно не было создано", "Error", MB_OK | MB_ICONERROR);
+		 return 0;
+	 }
+
+	//3 Запуск цикла сообщенией
+
+MSG msg;
+while (GetMessage(&msg, NULL, 0, 0) > 0)
+{
+	TranslateMessage(&msg);//переведённое сообщение
+	DispatchMessage(&msg); //отправленное сообщение
+}
+return msg.wParam;
+}
+
+
+
+
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY: PostQuitMessage(0); break;
+	case WM_CLOSE:	DestroyWindow(hwnd); break;
+	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return 0;
+
+	
+}
